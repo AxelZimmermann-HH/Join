2b80734f15500598fa2eb0706890cd51af749205
Ogulcan Erdag <ogulcanerdag@gmail.com>
2024-06-30 14:13:19 +0200

    adjusment

---
+++ privacy_policy.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const userContent = document.getElementById('user-content');
+    if (userContent.style.display === 'block') {
+        userContent.style.display = 'none';
+    } else {
+        userContent.style.display = 'block';
+    }
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('#user-logo')) {
+        const userContent = document.getElementById('user-content');
+        if (userContent.style.display === 'block') {
+            userContent.style.display = 'none';
+        }
+    }
+}
