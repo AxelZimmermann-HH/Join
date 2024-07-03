ba41231312c09cc8394a3f0e7882c797522ce525
AxelZimmermann-HH <axel.zimmermann89@gmail.com>
2024-07-03 11:26:33 +0200

    delete contact function

--- js/contacts.js
+++ js/contacts.js
@@ -3,23 +3,64 @@ const BASE_URL =
 
 let contactsData = {}; 
 let contactsArray = [];
+let contactsKeys = [];
+
 
 async function fetchDataJson() {
   try {
     let response = await fetch(BASE_URL + ".json");
     let responseToJson = await response.json();
-    contactsData = responseToJson; 
-    console.log(contactsData);
-    contactsArray = Object.values(contactsData); 
-    contactsArray.sort((a, b) => a.name.split(' ')[0].localeCompare(b.name.split(' ')[0])); 
-    console.log(contactsArray);
+    contactsData = responseToJson || {}; // Sicherstellen, dass contactsData ein Objekt ist
+    contactsArray = Object.values(contactsData);
+    contactsKeys = Object.keys(contactsData);
+
+    // Kontakte und Schlüssel gemeinsam sortieren
+    const sortedContacts = contactsArray.map((contact, index) => ({ contact, key: contactsKeys[index] }))
+                                        .sort((a, b) => a.contact.name.split(' ')[0].localeCompare(b.contact.name.split(' ')[0]));
+
+    // Sortierte Kontakte und Schlüssel zuweisen
+    contactsArray = sortedContacts.map(item => item.contact);
+    contactsKeys = sortedContacts.map(item => item.key);
 
     createContactsList(); 
-    return true; // Hinzugefügt, um anzuzeigen, dass die Daten erfolgreich geladen wurden
+    return true; // Daten erfolgreich geladen
 
   } catch (error) {
     console.error("Error fetching data:", error);
-    return false; // Hinzugefügt, um anzuzeigen, dass ein Fehler aufgetreten ist
+    return false; // Fehler aufgetreten
+  }
+}
+
+
+function createContactsList() {
+  let content = document.getElementById('contacts-list');
+  content.innerHTML = '';
+  let currentLetter = '';
+
+  for (let i = 0; i < contactsArray.length; i++) {
+      let contact = contactsArray[i];
+      let key = contactsKeys[i];
+      let nameParts = contact.name.split(' ');
+      let initials = nameParts[0][0] + nameParts[1][0];
+      let firstLetter = nameParts[0][0].toUpperCase();
+
+      if (firstLetter !== currentLetter) {
+          currentLetter = firstLetter;
+          content.innerHTML += `
+              <div class="first-letter">${currentLetter}</div>
+              <div class="line"></div>
+          `;
+      }
+
+      content.innerHTML += `
+          <div id="contact${key}" onclick='showContact("${initials}", ${JSON.stringify(contact)}, "${key}")' class="contact">
+              <div class="contact-letters">${initials}</div>
+              <div class="contact-data">
+                  <div class="contact-name">${contact.name}</div>
+                  <div class="contact-mail">${contact.email}</div>
+              </div>
+          </div>
+      `;
   }
 }
 
@@ -65,93 +106,80 @@ async function addContact() {
 }
 
 
-function createContactsList() {
-    let content = document.getElementById('contacts-list');
-    content.innerHTML = '';
-    let currentLetter = '';
+async function deleteContact(key) {
+  try {
+    let response = await fetch(BASE_URL + key + ".json", {
+      method: "DELETE",
+    });
+    await response.json();
+    fetchDataJson(); 
 
-    for (let i=0 ; i<contactsArray.length; i++) {
-        let contact = contactsArray[i];
-        let nameParts = contact.name.split(' ');
-        let initials = nameParts[0][0] + nameParts[1][0];
-        let firstLetter = nameParts[0][0].toUpperCase();
-        
-        if (firstLetter !== currentLetter) {
-            currentLetter = firstLetter;
-            content.innerHTML += `
-                <div class="first-letter">${currentLetter}</div>
-                <div class="line"></div>
-            `;
-        }
+    document.getElementById('contact-profile').innerHTML = '';
 
-        content.innerHTML += `
-            <div id="contact${i}" onclick='showContact("${initials}", ${JSON.stringify(contact)}, ${i})' class="contact">
-                <div class="contact-letters">${initials}</div>
-                <div class="contact-data">
-                    <div class="contact-name">${contact.name}</div>
-                    <div class="contact-mail">${contact.email}</div>
-                </div>
-            </div>
-        `
-    }
-}
-
-function showContact(initials, contact, i) {
-    highlightContact(i);
-    
-    let content = document.getElementById('contact-profile');
-    content.innerHTML = '';
-
-    content.classList.remove('slide-in-right'); // Reset animation
-    void content.offsetWidth; // Trigger reflow
-    content.classList.add('slide-in-right');
-
-    content.innerHTML += generateContactHTML(initials, contact)
+  } catch (error) {
+    console.error("Error deleting contact:", error);
+  }
 }
 
 
-function highlightContact(i) {
-    let contacts = document.getElementsByClassName('contact');
-    
-    for (let j = 0; j < contacts.length; j++) {
-        contacts[j].classList.remove('selected-contact');
-    }
+function showContact(initials, contact, key) {
+  highlightContact(key);
 
-    let currentContact = document.getElementById(`contact${i}`);
-    currentContact.classList.add('selected-contact');
+  let content = document.getElementById('contact-profile');
+  content.innerHTML = '';
 
+  content.classList.remove('slide-in-right'); // Reset animation
+  void content.offsetWidth; // Trigger reflow
+  content.classList.add('slide-in-right');
+
+  content.innerHTML += generateContactHTML(initials, contact, key);
 }
 
 
-function generateContactHTML(initials, contact) {
-    return `
-        <div class="contact-profile-firstrow">
-          <div class="contact-letters-big">${initials}</div>
-          <div class="contact-profile-firstrow-right">
-            <h3>${contact.name}</h3>
-            <div class="contact-actions">
-              <a class="contact-links">
-                <img class="contact-icon" src="img/contact-edit.svg" alt="">Edit
-              </a>
-              <a class="contact-links">
-                <img class="contact-icon" src="img/contact-delete.svg" alt="">Delete
-              </a>
-            </div>
+function highlightContact(key) {
+  let contacts = document.getElementsByClassName('contact');
+
+  for (let j = 0; j < contacts.length; j++) {
+      contacts[j].classList.remove('selected-contact');
+  }
+
+  let currentContact = document.getElementById(`contact${key}`);
+  if (currentContact) {
+      currentContact.classList.add('selected-contact');
+  }
+}
+
+
+function generateContactHTML(initials, contact, key) {
+  return `
+      <div class="contact-profile-firstrow">
+        <div class="contact-letters-big">${initials}</div>
+        <div class="contact-profile-firstrow-right">
+          <h3>${contact.name}</h3>
+          <div class="contact-actions">
+            <a class="contact-links">
+              <img class="contact-icon" src="img/contact-edit.svg" alt="">Edit
+            </a>
+            <a onclick="deleteContact('${key}')" class="contact-links">
+              <img class="contact-icon" src="img/contact-delete.svg" alt="">Delete
+            </a>
           </div>
         </div>
+      </div>
 
-        <p class="padding-top-bottom-27">Contact Information</p>
+      <p class="padding-top-bottom-27">Contact Information</p>
 
-        <div class="contact-channels">
-          <p>Email</p>
-          <a href="#">${contact.email}</a>
-        </div>
-        <div class="contact-channels">
-          <p>Phone</p>
-          <a class="black-link" href="#">${contact.phone}</a>
-        </div>
-    `
+      <div class="contact-channels">
+        <p>Email</p>
+        <a href="#">${contact.email}</a>
+      </div>
+      <div class="contact-channels">
+        <p>Phone</p>
+        <a class="black-link" href="#">${contact.phone}</a>
+      </div>
+  `;
 }
+
 
 function openAddContactLayer() {
     document.getElementById('add-contact-layer').classList.remove('d-none');
@@ -162,6 +190,7 @@ function openAddContactLayer() {
     void content.offsetWidth; 
     content.classList.add('slide-in-right');
 }
+
 
 function closeAddContactLayer() {
     let contentLayer = document.getElementById('add-contact-layer');
