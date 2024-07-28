fd69ae2e3700d75803e2887046dce1479bc65341
Ogulcan Erdag <ogulcanerdag@gmail.com>
2024-07-28 21:56:56 +0200

    privacy_policy jsdoc

--- privacy_policy.js
+++ privacy_policy.js
@@ -1,3 +1,10 @@
+
+
+/**
+ * Toggles the display of the user content menu.
+ * If the user content is currently displayed, it hides it.
+ * If the user content is currently hidden, it shows it.
+ */
 function toggleMenu() {
     const userContent = document.getElementById('user-content');
     if (userContent.style.display === 'block') {
@@ -7,6 +14,10 @@ function toggleMenu() {
     }
 }
 
+/**
+ * Hides the user content menu if the user clicks outside of it.
+ * @param {Event} event - The event triggered by clicking on the window.
+ */
 window.onclick = function(event) {
     if (!event.target.matches('#user-logo')) {
         const userContent = document.getElementById('user-content');
